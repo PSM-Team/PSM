7e459c9f4fbba17f41631805823acf8c8dadbc49
Jon Ervin <balmacedajonervin@gmail.com>
2017-09-18 23:53:04 +0800

    Register(Incomplete2)

--- app/modules/register/routes.js
+++ app/modules/register/routes.js
@@ -1,8 +1,9 @@
 var auth = require( './auth')
 var express = require('express');
 var router = express.Router();
-var arr = [], rand = [];
+var questArray = [], rand = [];
 var registerOn = 0;
+var tempRegister = {studnum:"" , studname:"", gender:"", bday:"", branch:"", email:"", contact:"", password:""};
 
 function flog(req, res, next){
     var db = require('../../lib/database')();
@@ -14,15 +15,15 @@ function flog(req, res, next){
 function fquestrand1(req, res, next){
     var db = require('../../lib/database')();
     db.query("SELECT * FROM tblquestions WHERE boolUsed= '0'", (err, results1, fields) => {
-      arr = [];
+      questArray = [];
       rand = [];
       if (err) console.log(err);
-      for(x=0;x<results1.length;x++){
-        arr[x] = results1[x].intQuestionID;
+      for(count=0;count<results1.length;count++){
+        questArray[count] = results1[count].intQuestionID;
       }
-      for(x=0;x<2;x++){
-        rand[x]= arr[Math.floor(Math.random()*arr.length)]
-        arr.splice(arr.indexOf(rand[x]),1);
+      for(count=0;count<2;count++){
+        rand[count]= questArray[Math.floor(Math.random()*questArray.length)]
+        questArray.splice(questArray.indexOf(rand[count]),1);
       }
       console.log(rand);
       return next();
@@ -30,8 +31,8 @@ function fquestrand1(req, res, next){
 }
 function fquestrand2(req, res, next){
     var db = require('../../lib/database')();
-    for(x=0;x<2;x++){
-      db.query("UPDATE tblquestions SET boolUsed= '1' WHERE intQuestionID= '"+ rand[x] +"'", (err, results2, fields) => {
+    for(count=0;count<2;count++){
+      db.query("UPDATE tblquestions SET boolUsed= '1' WHERE intQuestionID= ? ",[rand[count]], (err, results2, fields) => {
         if (err) console.log(err);
         return next();
       });
@@ -42,6 +43,9 @@ function fquestions(req, res, next){
     var db = require('../../lib/database')();
     db.query("SELECT * FROM tblquestions WHERE boolUsed= '1'", (err, results, fields) => {
       if (err) console.log(err);
+      for(count= 0;count<results.length;count++){
+        results[count].itemnum = count + 1;
+      }
       req.questions = results;
       return next();
     });
@@ -68,30 +72,39 @@ function render(req,res){
     res.render('register/views/index');
 }
 function CHrender(req,res){
-    res.render('register/views/questions', { questiontab: req.questions , choicetab: req.choices });
+    console.log(req.questions);
+    res.render('register/views/questions', { questiontab: req.questions , choicetab: req.choices,  });
+}
+function FINrender(req,res){
+    res.render('register/views/regformfin');
 }
 
 router.get('/', flog, render);
-router.get('/test', fquestrand1, fquestrand2, fquestions, fchoices, fcleanse, CHrender);
+router.get('/test', flog, fquestrand1, fquestrand2, fquestions, fchoices, fcleanse, CHrender);
+router.get('/fin', flog, FINrender);
 
 router.post('/',auth, (req, res) => {
     var db = require('../../lib/database')();
     if(req.body.password === req.body.confirm && req.body.password != ""){
-    db.query("INSERT INTO tbluser (strSNum, strName, strGender, datBirthday, strBranch, strEmail, txtContact, strPassword, strStatus, intCommend, intReport, strType, boolLoggedin) VALUES ('"+req.body.studnum+"','"+req.body.studname+"','"+req.body.gender+"','"+req.body.bday+"','"+req.body.branch+"','"+req.body.email+"','"+req.body.contact+"','"+req.body.password+"','unregistered','0','0','normal','0')", (err, results, fields) => {
-        if (err) {
-          console.log(err);
-          res.redirect('/register');
-        }
-        res.redirect('/register/test');
-    });
+    tempRegister.studnum = req.body.studnum;
+    tempRegister.studname = req.body.studname;
+    tempRegister.gender = req.body.gender;
+    tempRegister.bday = req.body.bday;
+    tempRegister.branch = req.body.branch;
+    tempRegister.email = req.body.email;
+    tempRegister.contact = req.body.contact;
+    tempRegister.password = req.body.password;
+    res.redirect('/register/test');
     }
     else{
       res.redirect('/register');
     }
+    console.log(tempRegister);
 });
 router.post('/test', (req, res) => {
-    var db = require('../../lib/database')();
-
+    console.log("POST VALUE");
+    console.log(req.body);
+    res.redirect('/register/fin');
 });
 
 exports.register = router;

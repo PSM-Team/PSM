1da24ebfc7c2fc6b43b10b29c02be4ce63e0a198
Jon Ervin <balmacedajonervin@gmail.com>
2017-09-20 03:50:26 +0800

    Home navbar

--- app/modules/home/routes.js
+++ app/modules/home/routes.js
@@ -19,4 +19,15 @@ function render(req,res){
 
 router.get('/', fuser, render);
 
+router.get('/contact_us', (req, res) => {
+    res.render('home/views/contact_us');
+});
+router.get('/help', (req, res) => {
+    res.render('home/views/help');
+});
+router.get('/rules', (req, res) => {
+    res.render('home/views/rules');
+});
+
+
 exports.home= router;

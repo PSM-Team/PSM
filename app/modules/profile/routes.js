1050bf147d976ed61fdb281f052159d3c79a04c0
Lance Jeron San Pablo <lancejeron26@gmail.com>
2017-09-23 19:12:07 +0800

    tried to work on profile(failed)
    some of the old codes are at in comments.

--- app/modules/profile/routes.js
+++ app/modules/profile/routes.js
@@ -2,43 +2,63 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('profile/views/index');
+    if (typeof process.env.ENABLE_DATABASE !== 'undefined' && process.env.ENABLE_DATABASE === 'false') {
+        
+            return render([]);
+        }
+    
+        var db = require('../../lib/database')();
+    
+        db.query('SELECT * FROM tblUser', function (err, results, fields) {
+    
+            if (err) return res.send(err);
+    
+            render(results);
+        });
+        
+        function render(tblUser) {
+            res.render('profile/views/index', { user: tblUser });
+        }
+    
 });
+
 router.get('/editprofile', (req, res) => {
     res.render('profile/views/editprofile');
-});router.post('/', (req, res) => {
-    var post  = req.body;
-    var name= post.user_name;
+
+});
+// router.post('/', (req, res) => {
+//     var post  = req.body;
+//     var name= post.user_name;
   
-  console.log(post);
-  console.log(name);
-  console.log(req.files);
-  if (!req.files)
-      return res.status(400).send('No files were uploaded.');
+//   console.log(post);
+//   console.log(name);
+//   console.log(req.files);
+//   if (!req.files)
+//       return res.status(400).send('No files were uploaded.');
   
-  var file = req.files.uploaded_image;
-  var img_name=file.name;
+//   var file = req.files.uploaded_image;
+//   var img_name=file.name;
   
-     if(file.mimetype == "image/jpeg" ||file.mimetype == "image/png"||file.mimetype == "image/gif" ){
+//      if(file.mimetype == "image/jpeg" ||file.mimetype == "image/png"||file.mimetype == "image/gif" ){
   
-            file.mv('public/images/upload_images/'+file.name, function(err) {
+//             file.mv('public/images/upload_images/'+file.name, function(err) {
   
-              if (err)
+//               if (err)
   
-                return res.status(500).send(err);
-              var db = require('../../lib/database')();
-              var sql = "INSERT INTO `users_image`(`user_name`,`image`) VALUES ('"+name+"','"+img_name+"')";
-              var query = db.query(sql, function(err, result) {
-                 res.render('profle/views/editprofile');
-              });
-           });
-        } else {
-          message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
-          res.render('editprofile.pug',{message: message});
-  };
-  });
+//                 return res.status(500).send(err);
+//               var db = require('../../lib/database')();
+//               var sql = "INSERT INTO `users_image`(`user_name`,`image`) VALUES ('"+name+"','"+img_name+"')";
+//               var query = db.query(sql, function(err, result) {
+//                  res.render('profle/views/editprofile');
+//               });
+//            });
+//         } else {
+//           message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
+//           res.render('editprofile.pug',{message: message});
+//   };
+//   });
   
 
 
-exports.editprofile= router;
+//exports.editprofile= router;
 exports.profile= router;
